f58e86e631875b522a00e2bb174ba32dc9ad2cea
Jungbin Choi <127074334+j1b2c3@users.noreply.github.com>
2026-03-12 09:18:40 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.63 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int div(int a){
-    int count = 0;
+    int count = 1;
     
     for (int i = 1; i <= a/2; i++){
         if (a % i == 0)
@@ -18,9 +18,9 @@ int solution(int left, int right) {
     int answer = 0;
     for (int i = left; i <= right; i++){
         if (div(i) % 2 == 0)
-            answer -= i;
-        else
             answer += i;
+        else
+            answer -= i;
     }
     return answer;
 }
