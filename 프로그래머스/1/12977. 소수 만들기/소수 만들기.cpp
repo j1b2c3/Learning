16296f958ecdaebfc6e79c1b683cb66c53a06683
Jungbin Choi <127074334+j1b2c3@users.noreply.github.com>
2026-04-10 09:31:45 +0900

    [level 1] Title: 소수 만들기, Time: 33.67 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums) {
+    int answer = 0;
+        
+    for (int i = 0; i < nums.size() - 2; i++){
+        for (int j = i + 1; j < nums.size() - 1; j++){
+            for (int k = j + 1; k < nums.size(); k++){
+                int sum = nums[i] + nums[j] + nums[k];
+                bool isprime = true;
+                for (int l = 2; l < (sum / 2); l++){
+                    if (sum % l == 0)
+                        isprime = false;
+                }
+                if (isprime)
+                    answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
