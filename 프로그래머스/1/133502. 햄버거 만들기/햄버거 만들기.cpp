7bba5d883c4d65c51923aaefd8eb9bbed9c0b144
Jungbin Choi <127074334+j1b2c3@users.noreply.github.com>
2026-04-24 09:47:29 +0900

    [level 1] Title: 햄버거 만들기, Time: 11.93 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    vector<int> stack;
+    
+    for (int i : ingredient){
+        stack.push_back(i);
+        
+        if (stack.size() >= 4){
+            int n = stack.size();
+            
+            if (stack[n-4] == 1 && stack[n-3] == 2 && stack [n-2] == 3 && stack [n-1] == 1){
+                stack.pop_back();
+                stack.pop_back();                
+                stack.pop_back();
+                stack.pop_back();
+
+                answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
